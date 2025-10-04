f16462b857c06b856213455a0f8bf026ae10eacb
sanjayram-07 <m.sanjayram07@gmail.com>
2025-10-04 22:15:00 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <GoogleOAuthProvider clientId={clientId}>
     <App />
